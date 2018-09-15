59ed434823a1be6dfd4fe352e35c477fba0dd30a
Shirley Leu <sleu@uw.edu>
2018-09-15 13:01:26 +0200

    Download conversation transcript to text file Create filename from URL date

--- foxtrot.py
+++ foxtrot.py
@@ -1,30 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-import re
 import time
 
-base_url = 'https://www.gocomics.com'
 
-# Start URL
-url = '/foxtrot/1988/05/04'
-
-while True:
-    page = requests.get(base_url + url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    select_div = soup.select_one('div[data-image]')
-    image_url = select_div.attrs['data-image']
-    image_response = requests.get(image_url)
-
-    if image_response.status_code == 200:
-        content_disp = image_response.headers['Content-Disposition']
-        filename = re.findall('filename=(.+)', content_disp)
-        formatted_filename = str(filename[0]).strip('\"')
-        with open('/Users/shirley/Desktop/foxtrot/%s' % formatted_filename, 'wb') as f:
-            f.write(image_response.content)
-
-    # Finds the next URL from the navigation button
-    select_next_div = soup.find('a', "fa btn btn-outline-secondary btn-circle fa-caret-right sm ")
-    if select_next_div == None:
-        break
-    url = select_next_div.attrs['href']
-    time.sleep(5)
+def download_images(start_url):
+    base_url = 'https://www.gocomics.com'
+    url = start_url
+    while True:
+        page = requests.get(base_url + url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        # Filename is created using URL date
+        formatted_date = str(url).lstrip('/foxtrot').replace('/', '-')
+        # The div block that has all the good stuff
+        select_image_div = soup.select_one('div[data-image]')
+        # Transcript collected here
+        transcript = select_image_div.attrs['data-transcript']
+        with open('/Users/shirley/Desktop/foxtrot/%s' % (formatted_date + '.txt'), 'w') as f:
+            f.write(transcript)
+        # Image URL collected here first
+        image_url = select_image_div.attrs['data-image']
+        image_response = requests.get(image_url)
+        # Image downloaded here
+        if image_response.status_code == 200:
+            with open('/Users/shirley/Desktop/foxtrot/%s' % (formatted_date + '.gif'), 'wb') as f:
+                f.write(image_response.content)
+        # Finds the next URL from the navigation button
+        select_next_div = soup.find('a', "fa btn btn-outline-secondary btn-circle fa-caret-right sm ")
+        if select_next_div == None:
+            break
+        url = select_next_div.attrs['href']
+        time.sleep(5)
