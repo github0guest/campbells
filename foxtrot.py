2bfe8c9fdad34bc01a94d8f381257f66ab5f4f35
Shirley Leu <sleu@uw.edu>
2018-09-19 11:14:56 +0200

    Refactor code, Remove extraneous insert_row DB function

--- foxtrot.py
+++ foxtrot.py
@@ -1,5 +1,3 @@
-import ssl
-
 from bs4 import BeautifulSoup
 import requests
 import time
@@ -15,7 +13,6 @@ def warm_up_soup(start_url):
     date_url = start_url
     with open(download_directory + "missing_images.txt", 'w') as f:
         while True:
-            print(date_url)
             time.sleep(5)
             try:
                 page = requests.get(base_url + date_url)
@@ -23,33 +20,33 @@ def warm_up_soup(start_url):
                 print(ex)
                 print("Error on date: %s" % date_url)
                 continue
+            # Page parsed for image URL and transcript
             soup = BeautifulSoup(page.text, 'html.parser')
-            # The div block that has all the good stuff
             select_image_div = soup.select_one('div[data-image]')
-            # Transcript collected here
             transcript = select_image_div.attrs['data-transcript']
-            # Image URL collected here first
             image_url = select_image_div.attrs['data-image']
+            # Missing images caught and dates noted
             if image_url == "https://assets.gocomics.com/content-error-missing-image.jpeg":
                 f.write(date_url + "\n")
                 print("Error: Missing image")
                 try:
-                    date_url = find_next_image(soup)
+                    date_url = find_next_date(soup)
                 except DoneException:
                     break
                 continue
+            # Otherwise, image URL is considered valid and eventually downloaded/DB updated
             image_response = requests.get(image_url)
             if image_response.status_code == 200:
                 download_image_update_db(date_url, image_response, transcript)
             try:
-                date_url = find_next_image(soup)
+                date_url = find_next_date(soup)
             except DoneException:
                 break
     conn.close()
 
 
-def find_next_image(soup):
-    # Finds the next URL from the navigation button
+def find_next_date(soup):
+    """Find the next date's URL from the navigation button."""
     select_next_div = soup.find('a', "fa btn btn-outline-secondary btn-circle fa-caret-right sm ")
     if select_next_div is None:
         raise DoneException()
@@ -57,23 +54,20 @@ def find_next_image(soup):
 
 
 def download_image_update_db(url, image_response, transcript):
+    """Download image locally and inserts entry in DB."""
     # Date in YYYY/mm/dd format
     date_slash = str(url.lstrip('/foxtrot/'))
     # Image downloaded here
     with open(download_directory + datetime.strptime(date_slash, '%Y/%m/%d').strftime('%Y-%m-%d') + '.gif', 'wb') as f:
         f.write(image_response.content)
-    # Insert the date and transcript into database
     # Convert date to Unix time
-    date = datetime.strptime(date_slash, '%Y/%m/%d')
-    unix_time = datetime(date.year, date.month, date.day, tzinfo=timezone.utc).timestamp()
-    insert_row(unix_time, transcript, conn)
-
-
-def insert_row(date, transcript, connection):
-    c = connection.cursor()
-    values = (date, transcript)
+    dt_obj = datetime.strptime(date_slash, '%Y/%m/%d')
+    unix_time = datetime(dt_obj.year, dt_obj.month, dt_obj.day, tzinfo=timezone.utc).timestamp()
+    # Insert the date and transcript into database
+    c = conn.cursor()
+    values = (unix_time, transcript)
     c.execute('REPLACE INTO comics VALUES (?,?)', values)
-    connection.commit()
+    conn.commit()
 
 
 class DoneException(Exception):
