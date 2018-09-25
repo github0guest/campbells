769c88f334227b5d971f9b2fc429bd4e34615473
Shirley <shirley.w.leu@gmail.com>
2018-09-25 21:28:54 +0200

    Fix except clause continue to pass

--- characters.py
+++ characters.py
@@ -1,6 +1,6 @@
 import sqlite3
 import re
-from operator import itemgetter
+from datetime import timezone, datetime
 
 conn = sqlite3.connect('foxtrot.db')
 c = conn.cursor()
@@ -10,26 +10,39 @@ characters = {}
 
 
 def char_insertion():
-    c.execute('SELECT date, transcript FROM comic')
-    date_transcript = c.fetchall()
-    for row in date_transcript:
+    for row in c.execute('SELECT date, transcript FROM comic'):
         date = row[0]
-        m = p.findall(row[1])
-        for word in m:
+        transcript = p.findall(row[1])
+        for word in transcript:
             name = str(word).rstrip(":")
+            c2 = conn.cursor()
             try:
-                c.execute('INSERT INTO character(first_name, last_name) VALUES(?,?)', (name, ""))
+                c2.execute('INSERT INTO character(first_name, last_name) VALUES(?,?)', (name, ""))
             except sqlite3.IntegrityError as err:
                 print(err)
-                continue
-            c.execute('SELECT id FROM character WHERE first_name = ?', (name, ))
-            char_id = c.fetchone()
+                pass
+            c2.execute('SELECT id FROM character WHERE first_name = ?', (name, ))
+            char_row = c2.fetchone()
             try:
-                c.execute('INSERT INTO comic_character(comic_date, character_id) VALUES (?,?)', (date, char_id[0]))
+                c2.execute('INSERT INTO comic_character(comic_date, character_id) VALUES (?,?)', (date, char_row[0]))
             except sqlite3.IntegrityError as err:
                 print(err)
-                continue
+                pass
     conn.commit()
     conn.close()
 
-char_insertion()
+
+def characters_from_date(date):
+    dt_obj = datetime.strptime(date, '%Y-%m-%d')
+    unix_time = datetime(dt_obj.year, dt_obj.month, dt_obj.day, tzinfo=timezone.utc).timestamp()
+    names = []
+    for row in c.execute('SELECT character_id FROM comic_character WHERE comic_date = ?', (int(unix_time), )):
+        c2 = conn.cursor()
+        c2.execute('SELECT first_name FROM character WHERE id = ?', (row[0],))
+        name = c2.fetchone()
+        names.append(name[0])
+    return sorted(names, key=str.lower)
+
+
+if __name__ == "__main__":
+    characters_from_date("1988-08-03")
