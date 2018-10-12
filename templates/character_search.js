7f4b3bc01ca166182b1883bbb2a10bf223dd4fa1
Shirley <shirley.w.leu@gmail.com>
2018-10-12 18:33:15 +0200

    Change general search API to POST

---
+++ templates/character_search.js
@@ -0,0 +1,50 @@
+new Vue({
+    el: '#character',
+    data: {
+        info: null,
+        loading: true,
+        errored: false,
+        newCharacter: 'Eileen',
+    },
+    methods: {
+        enterCharacter(character) {
+            this.newCharacter = character;
+            axios
+                .get('http://127.0.0.1:5000/json/dates/?character=' + this.newCharacter )
+                .then(response => (this.info = response.data))
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        },
+        renderComic(date) {
+            return date + `<img src="http://127.0.0.1:5000/static/images/` + date + `.gif">`
+        }
+    },
+});
+
+new Vue({
+    el: '#search',
+    data: {
+        info: null,
+        loading: true,
+        errored: false,
+        searchTerm: 'Skeeter Falls',
+    },
+    methods: {
+        search(term) {
+            this.newSearchTerm = term;
+            axios.post('http://127.0.0.1:5000/json/search/', JSON.stringify({text: this.searchTerm}))
+                .then(response => (this.info = response.data))
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        },
+        renderComic(date) {
+            return date + `<img src="http://127.0.0.1:5000/static/images/` + date + `.gif">`
+        }
+    },
+});
