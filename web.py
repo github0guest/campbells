ecb5a945158146dc66ecee9dc3e8edbbbf3478fd
Shirley <shirley.w.leu@gmail.com>
2018-10-14 13:16:05 +0200

    Remove html template routes

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from storage import ComicManager
 
@@ -6,37 +6,52 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/json/characters/<date>')
-def json_characters(date):
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route('/json/characters/')
+def json_characters():
+    date = request.args.get('date')
     cm = ComicManager('foxtrot.db')
     return jsonify(cm.characters_from_date(date))
 
 
-@app.route('/json/dates/<character>')
-def json_dates(character):
+@app.route('/json/dates/')
+def json_dates():
+    character = request.args.get('character')
     cm = ComicManager('foxtrot.db')
     return jsonify(cm.dates_from_character(character))
 
 
-@app.route('/json/search/<search_term>')
-def search(search_term):
+@app.route('/json/search/', methods=['POST'])
+def search():
+    content = request.get_json(force=True)
     cm = ComicManager('foxtrot.db')
-    return jsonify(cm.search_transcripts(search_term))
-
-
-@app.route('/')
-def home():
-    return render_template(
-        'search.html', title='Search')
-
-
-@app.route('/characters/')
-@app.route('/characters/<date>')
-def characters(date=None, char=None):
-    cm = ComicManager('foxtrot.db')
-    if date is not None:
-        char = cm.characters_from_date(date)
-    return render_template('characters.html', date=date, char=char, title="Which characters?")
+    try:
+        return jsonify(cm.search_transcripts(content['text']))
+    except KeyError as err:
+        print(err)
+        raise InvalidUsage('KeyError', status_code=400)
 
 
 if __name__ == '__main__':
