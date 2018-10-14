f2227a0043580f158a8512bc4b9e9c1e8ecdc6d2
Shirley <shirley.w.leu@gmail.com>
2018-10-14 13:13:25 +0200

    Add input for date entry returning character list

--- templates/character_search.js
+++ templates/character_search.js
@@ -24,6 +24,34 @@ new Vue({
     },
 });
 
+
+new Vue({
+    el: '#date',
+    data: {
+        info: null,
+        loading: true,
+        errored: false,
+        dateSearch: '1988-04-11',
+    },
+    methods: {
+        enterDate(date) {
+            this.dateSearch = date;
+            axios
+                .get('http://127.0.0.1:5000/json/characters/?date=' + this.dateSearch )
+                .then(response => (this.info = response.data))
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        },
+        renderComic(date) {
+            return `<img src="http://127.0.0.1:5000/static/images/` + this.dateSearch + `.gif">`
+        }
+    },
+});
+
+
 new Vue({
     el: '#search',
     data: {
