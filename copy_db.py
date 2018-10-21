acc2842666dcd76d0b67076296e3874cc81e5b0c
Shirley <shirley.w.leu@gmail.com>
2018-10-21 15:49:13 +0200

    Detach row from first session

--- copy_db.py
+++ copy_db.py
@@ -8,4 +8,5 @@ with cm_lite.session_scope() as s_lite:
     comics = s_lite.query(Comic)
     with cm_mysql.session_scope() as s_my:
         for comic in comics:
+            s_lite.expunge(comic)
             s_my.add(comic)
