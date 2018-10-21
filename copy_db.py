701e633cf893ee358a102ea056796a18a3f7f33d
Shirley <shirley.w.leu@gmail.com>
2018-10-21 16:02:04 +0200

    Fix DB copy script

--- copy_db.py
+++ copy_db.py
@@ -8,5 +8,5 @@ with cm_lite.session_scope() as s_lite:
     comics = s_lite.query(Comic)
     with cm_mysql.session_scope() as s_my:
         for comic in comics:
-            s_lite.expunge(comic)
-            s_my.add(comic)
+            new_comic = Comic(date=comic.date, transcript=comic.transcript)
+            s_my.add(new_comic)
