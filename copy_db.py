9b1d6e97ceb7fb900e7cb8171ef356b6f68bacb6
Shirley <shirley.w.leu@gmail.com>
2018-10-21 15:28:34 +0200

    Add script to copy Comic table from SQLite to MySQL

---
+++ copy_db.py
@@ -0,0 +1,11 @@
+from storage import ComicManagerAlchemy
+from models import Comic
+
+cm_lite = ComicManagerAlchemy('sqlite:///foxtrot.db')
+cm_mysql = ComicManagerAlchemy('mysql://gregoria:moomie11@gregoria.mysql.pythonanywhere-services.com/gregoria$foxtrot')
+
+with cm_lite.session_scope() as s_lite:
+    comics = s_lite.query(Comic)
+    with cm_mysql.session_scope() as s_my:
+        for comic in comics:
+            s_my.add(comic)
